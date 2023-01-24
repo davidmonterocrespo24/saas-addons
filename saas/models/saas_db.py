1f7375087d65a4e88207393064bb41e32b6e628b
david <davidmonterocrespo24@gmail.com>
2023-01-24 11:51:41 -0300

    mejoras

--- saas/models/saas_db.py
+++ saas/models/saas_db.py
@@ -90,6 +90,7 @@ class SAASDB(models.Model):
         for build in self.filtered(lambda x: x.state == "done"):
             operator = build.operator_id
             operator._install_modules(build.name, [('name', 'in', operator.get_mandatory_modules())])
+
     def action_update_module_base(self):
         for build in self.filtered(lambda x: x.state == "done"):
             operator = build.operator_id
